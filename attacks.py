17d030659eaa56cdb93a474a8d4633a2c8ed3df0
JeongHyunLee <nomar0107@gmail.com>
2020-10-13 13:52:30 +0900

    run result

--- attacks.py
+++ attacks.py
@@ -2,17 +2,9 @@ from advertorch.attacks import LinfPGDAttack
 import torch.nn as nn
 import torch
 import torch.nn.functional as F
-import numpy as np
-import random
 
 
 def get_pgd_adversary(model, eps, num_iter, lr, rand_init, seed):
-
-    # torch.manual_seed(seed)
-    # torch.cuda.manual_seed(seed)
-    # np.random.seed(seed)
-    # random.seed(seed)
-    # torch.backends.cudnn.deterministic = True
 
     adversary = LinfPGDAttack(model, loss_fn=nn.CrossEntropyLoss(reduction="sum"), eps=eps, nb_iter=num_iter,
                               rand_init=rand_init, eps_iter=lr, clip_min=0.0, clip_max=1.0)
@@ -20,11 +12,6 @@ def get_pgd_adversary(model, eps, num_iter, lr, rand_init, seed):
 
 
 def pgd_attack(model, input, target, eps, num_iter, lr, rand_init, seed):
-    # torch.manual_seed(seed)
-    # torch.cuda.manual_seed(seed)
-    # np.random.seed(seed)
-    # random.seed(seed)
-    # torch.backends.cudnn.deterministic = True
 
     x = input.detach()
 
